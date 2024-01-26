dcfce8226c32569f132b26b4c1d94a66cfabca39
GarciaGP <gabriel.garp@outlook.com>
2024-01-25 23:39:15 -0300

    correção de import no módulo de pagamento

--- src/pagamento/pagamento.module.ts
+++ src/pagamento/pagamento.module.ts
@@ -3,9 +3,10 @@ import { PagamentoService } from "./services";
 import { pagamentoProviders } from "./pagamento.providers";
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PagamentoMongoDBRepositoryGateway } from "./gateways/PagamentoMongoDBRepositoryGateway";
+import { PagamentoModel } from "./gateways";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([PagamentoModule])],
+    imports: [TypeOrmModule.forFeature([PagamentoModel])],
     controllers: [PagamentoService],
     providers: [...pagamentoProviders, PagamentoMongoDBRepositoryGateway]
   })
